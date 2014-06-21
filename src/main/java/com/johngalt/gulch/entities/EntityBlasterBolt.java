c3cc5c8bcfdffaea04d48f9e836eec069d506060
horvay <horvay@gmail.com>
2014-06-21 14:25:12 -0400

    Implemented recipes and started creating reload recipes.

--- src/main/java/com/johngalt/gulch/entities/EntityBlasterBolt.java
+++ src/main/java/com/johngalt/gulch/entities/EntityBlasterBolt.java
@@ -1,54 +1,29 @@
 package com.johngalt.gulch.entities;
 
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 /**
  * Created on 6/14/2014.
  */
-public class EntityBlasterBolt extends EntityThrowable
+public class EntityBlasterBolt extends GaltAmmoEntity
 {
-    int tickCount = -1;
-
-    public EntityBlasterBolt(World par1World)
+    // DO NOT USE. INTERNAL USE ONLY
+    public EntityBlasterBolt(World world)
     {
-        super(par1World);
+        super(world);
     }
 
-    public EntityBlasterBolt(World par1World, EntityLivingBase par2EntityLiving)
+    public EntityBlasterBolt(World world, EntityLivingBase entityLiving)
     {
-        super(par1World, par2EntityLiving);
-
-        setThrowableHeading(this.motionX, this.motionY, this.motionZ, 3.0F, 1.0F);
+        super(world, entityLiving, (byte) 10, 0.0F);
     }
 
     @Override
-    public void onUpdate()
+    protected void onImpact(MovingObjectPosition objectPosition)
     {
-
-        super.onUpdate();
-
-        if (tickCount >= 0)
-        {
-            if (tickCount++ > 1)
-            {
-                this.setDead();
-            }
-        }
-    }
-
-    @Override
-    protected void onImpact(MovingObjectPosition var1)
-    {
+        super.onImpact(objectPosition);
         this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, 1.0F, true);
-        tickCount = 0;
-    }
-
-    @Override
-    protected float getGravityVelocity()
-    {
-        return 0;
     }
 }
